b8262f4c2ac10127c03f8979fcc47eb98727e3a3
Antonio Carlos <antonioccsjr@hotmail.com>
2024-04-06 16:43:05 -0300

    Alteração no link do CV

--- src/componentes/AboutComponentes/ButtonCVAbout/ButtonCVAbout.jsx
+++ src/componentes/AboutComponentes/ButtonCVAbout/ButtonCVAbout.jsx
@@ -3,7 +3,7 @@ import '../ButtonCVAbout/ButtonCVAbout.css';
 export default function ButtonCVAbout() {
     return (
         <div className="button-about">
-            <a className="button-cv-about" href="https://drive.google.com/file/d/1dZpyymt6bX8ZmODE0-mR8LMfP6YY7A2N/view?usp=drive_link" rel='noopener noreferrer' target="_blank">Download CV</a>
+            <a className="button-cv-about" href="https://drive.google.com/file/d/1mwJfEmpgAa5L3wco3LsLE6EBJgTwDHXn/view?usp=drive_link" rel='noopener noreferrer' target="_blank">Download CV</a>
         </div>
     )
 }
--- src/componentes/ButtonCV/ButtonCV.jsx
+++ src/componentes/ButtonCV/ButtonCV.jsx
@@ -3,7 +3,7 @@ import '../ButtonCV/ButtonCV.css'
 export default function ButtonCV() {
     return (
         <div className="button">
-            <a className="button-cv" href="https://drive.google.com/file/d/1dZpyymt6bX8ZmODE0-mR8LMfP6YY7A2N/view?usp=drive_link" rel='noopener noreferrer' target="_blank">Download CV</a>
+            <a className="button-cv" href="https://drive.google.com/file/d/1mwJfEmpgAa5L3wco3LsLE6EBJgTwDHXn/view?usp=drive_link" rel='noopener noreferrer' target="_blank">Download CV</a>
         </div>
     )
 }
