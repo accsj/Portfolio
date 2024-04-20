6724c6f18dbed4a68c69ba8b98c0827553ddab1b
Antonio Carlos <antonioccsjr@hotmail.com>
2024-04-20 19:16:40 -0300

    Alteração no texto about

--- src/componentes/AboutComponentes/SectionRight/SectionRight.jsx
+++ src/componentes/AboutComponentes/SectionRight/SectionRight.jsx
@@ -7,12 +7,12 @@ export default function Aside () {
             <h2>Sobre <span>Mim</span></h2>
             <h3>Desenvolvedor <span className="typewrite-about">Front-end</span></h3>
             <p>
-                Cresci em uma cidade bem pequena chamada Xerém localizada no Rio de Janeiro no município de Duque de Caxias, e desde muito novo sempre fui apaixonado por computadores e tecnologias no geral, porém apenas no 8° ano do
+                Cresci em uma cidade bem pequena chamada Xerém, localizada no Rio de Janeiro no município de Duque de Caxias, e desde muito novo sempre fui apaixonado por computadores e tecnologias no geral, porém apenas no 8° ano do
                 ensino fundamental que tive o meu primeiro contato com uma linguagem de programação, o Python.
             </p>
             <p>
-                Resolvi procurar formas em que o Python poderia ajudar no meu dia-a-dia, então enquanto fazia algumas lições de aula, acabei me deparando com uma tarefa repetitiva e chata, onde compreendia os conceitos de matemática porém
-                tinha preguiça de efetuar eles então pensei, por quê não criar uma aplicação para resolver equações de 1° e 2° grau? E foi exatamente o que fiz.
+                Resolvi procurar formas em que o Python poderia ajudar no meu dia-a-dia, então enquanto fazia algumas lições de aula, acabei me deparando com uma tarefa repetitiva e cansativa, onde compreendia os conceitos de matemática porém
+                tinha preguiça de efetuar então pensei, por quê não criar uma aplicação para resolver equações de 1° e 2° grau? E foi exatamente o que fiz.
             </p>
             <p>
                 Essa aplicação me ajudou muito, e desde então desenvolvi códigos com a minha lógica de programação para resolver problemas matemáticos utilizando Python.
